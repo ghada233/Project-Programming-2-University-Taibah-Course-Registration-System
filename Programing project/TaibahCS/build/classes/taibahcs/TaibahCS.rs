7a613fa87b11fedf033a1e49a05462c9e022840c
GhadaAljohani <70041510+ghada233@users.noreply.github.com>
2021-12-19 00:57:47 +0300

    Add files via upload

---
+++ Programing project/TaibahCS/build/classes/taibahcs/TaibahCS.rs
@@ -0,0 +1,1 @@
+Taibahcs.TaibahCS
---
+++ Programing project/TaibahCS/src/taibahcs/TAs.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taibahcs;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author M
+ */
+public class TAs extends FacultyMember {
+
+    private int maxmimumMumberOfCourse;
+    private int quotaOfCreditHourse;
+    private ArrayList<String> assignedCourses = new ArrayList<>();
+
+    public TAs() {
+    }
+
+    public TAs(int FacultyID, String firstName, String lastName, String academicRank, String academicSpecialization,
+            int maxmimumMumberOfCourse, int quotaOfCreditHourse ,ArrayList<String> s) {
+        super(FacultyID,firstName, lastName, academicRank, academicSpecialization);
+        this.maxmimumMumberOfCourse = maxmimumMumberOfCourse;
+        this.quotaOfCreditHourse = quotaOfCreditHourse;
+        this.assignedCourses=s;
+    }
+
+    public int getMaxmimumMumberOfCourse() {
+        return maxmimumMumberOfCourse;
+    }
+
+    public void setMaxmimumMumberOfCourse(int maxmimumMumberOfCourse) {
+        this.maxmimumMumberOfCourse = maxmimumMumberOfCourse;
+    }
+
+    public int getQuotaOfCreditHourse() {
+        return quotaOfCreditHourse;
+    }
+
+    public void setQuotaOfCreditHourse(int quotaOfCreditHourse) {
+        this.quotaOfCreditHourse = quotaOfCreditHourse;
+    }
+
+    public ArrayList<String> getAssignedCourses() {
+        return assignedCourses;
+    }
+
+    public void setAssignedCourses(ArrayList<String> s) {
+        this.assignedCourses = s;
+    }
+
+    @Override
+    public String toString() {
+       
+   return "FacultyID : " + super.getFacultyID()+ " firstName : " 
+                + super.getFirstName()  +" lastName : " + super.getLastName() + " academicRank : "
+                + super.getAcademicRank() + " academicSpecialization : " + super.getAcademicSpecialization()+ " maxmimumMumberOfCourse : " + maxmimumMumberOfCourse  +" quotaOfCreditHourse : "
+                + quotaOfCreditHourse +" assignedCourses : " + assignedCourses +"\n" ;
+    }
+
+}
